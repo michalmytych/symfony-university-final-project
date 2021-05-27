2cc5fcd72c0614d3cce34474741f4bf6d7cd4a22
mike <michamytych@gmail.com>
2021-05-27 22:20:36 +0200

    #33 add validation rules adnotations at Post entity

--- app/src/Entity/Post.php
+++ app/src/Entity/Post.php
@@ -7,6 +7,7 @@ use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Represents social network-like post added by user to specific course or group.
@@ -43,13 +44,15 @@ class Post
      * Title of post.
      *
      * @ORM\Column(type="string", length=128)
+     * @Assert\Type(type="string")
+     * @Assert\Length(min="3", max="128")
      */
     private $title;
 
     /**
      * Courses related to post.
      *
-     * @ORM\ManyToMany(targetEntity=Course::class, mappedBy="posts")
+     * @ORM\ManyToMany(targetEntity=Course::class, mappedBy="posts", fetch="EXTRA_LAZY")
      */
     private $courses;
 
